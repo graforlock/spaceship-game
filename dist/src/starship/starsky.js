fc556d0bd5e472a2a8e0e068b11ced52f62e0498
Maciej Sitko <maciej@neatcube.co.uk>
2016-05-03 22:46:16 +0100

    Enemies added

--- dist/src/starship/starsky.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var canvas_1 = require('../canvas/canvas');
-var StarSky = new canvas_1.default(window.innerWidth, window.innerHeight);
-StarSky.draw();
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = StarSky;
